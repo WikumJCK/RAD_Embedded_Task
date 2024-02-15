278419c65702695ccaf8c679afe54b4f4085eae6
Wikum Jayasanka <44830718+WikumJCK@users.noreply.github.com>
2024-02-15 05:49:30 +0530

    RTC and UART LED completed

--- RAD_Embedded_Task/Core/Inc/stm32f4xx_it.h
+++ RAD_Embedded_Task/Core/Inc/stm32f4xx_it.h
@@ -54,7 +54,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void DMA1_Stream1_IRQHandler(void);
 void DMA1_Stream2_IRQHandler(void);
-void DMA1_Stream3_IRQHandler(void);
+void DMA1_Stream4_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void USART3_IRQHandler(void);
 void DMA1_Stream7_IRQHandler(void);
