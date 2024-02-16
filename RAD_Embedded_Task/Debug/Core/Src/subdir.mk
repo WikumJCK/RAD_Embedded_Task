fdab00fdf23d233a0561d43ba1a85ccb410dde92
Wikum Jayasanka <44830718+WikumJCK@users.noreply.github.com>
2024-02-16 09:35:54 +0530

    All the codes combined with DS18b20

--- RAD_Embedded_Task/Debug/Core/Src/subdir.mk
+++ RAD_Embedded_Task/Debug/Core/Src/subdir.mk
@@ -15,13 +15,7 @@ C_SRCS += \
 ../Core/Src/stm32f4xx_it.c \
 ../Core/Src/syscalls.c \
 ../Core/Src/sysmem.c \
-../Core/Src/system_stm32f4xx.c \
-../Core/Src/tm_stm32_delay.c \
-../Core/Src/tm_stm32_disco.c \
-../Core/Src/tm_stm32_ds18b20.c \
-../Core/Src/tm_stm32_gpio.c \
-../Core/Src/tm_stm32_onewire.c \
-../Core/Src/tm_stm32_rcc.c 
+../Core/Src/system_stm32f4xx.c 
 
 OBJS += \
 ./Core/Src/ds18b20.o \
@@ -34,13 +28,7 @@ OBJS += \
 ./Core/Src/stm32f4xx_it.o \
 ./Core/Src/syscalls.o \
 ./Core/Src/sysmem.o \
-./Core/Src/system_stm32f4xx.o \
-./Core/Src/tm_stm32_delay.o \
-./Core/Src/tm_stm32_disco.o \
-./Core/Src/tm_stm32_ds18b20.o \
-./Core/Src/tm_stm32_gpio.o \
-./Core/Src/tm_stm32_onewire.o \
-./Core/Src/tm_stm32_rcc.o 
+./Core/Src/system_stm32f4xx.o 
 
 C_DEPS += \
 ./Core/Src/ds18b20.d \
@@ -53,13 +41,7 @@ C_DEPS += \
 ./Core/Src/stm32f4xx_it.d \
 ./Core/Src/syscalls.d \
 ./Core/Src/sysmem.d \
-./Core/Src/system_stm32f4xx.d \
-./Core/Src/tm_stm32_delay.d \
-./Core/Src/tm_stm32_disco.d \
-./Core/Src/tm_stm32_ds18b20.d \
-./Core/Src/tm_stm32_gpio.d \
-./Core/Src/tm_stm32_onewire.d \
-./Core/Src/tm_stm32_rcc.d 
+./Core/Src/system_stm32f4xx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -69,7 +51,7 @@ Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/ds18b20.d ./Core/Src/ds18b20.o ./Core/Src/ds18b20.su ./Core/Src/fatfs_sd.d ./Core/Src/fatfs_sd.o ./Core/Src/fatfs_sd.su ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/onewire.d ./Core/Src/onewire.o ./Core/Src/onewire.su ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_hal_timebase_tim.su ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/tm_stm32_delay.d ./Core/Src/tm_stm32_delay.o ./Core/Src/tm_stm32_delay.su ./Core/Src/tm_stm32_disco.d ./Core/Src/tm_stm32_disco.o ./Core/Src/tm_stm32_disco.su ./Core/Src/tm_stm32_ds18b20.d ./Core/Src/tm_stm32_ds18b20.o ./Core/Src/tm_stm32_ds18b20.su ./Core/Src/tm_stm32_gpio.d ./Core/Src/tm_stm32_gpio.o ./Core/Src/tm_stm32_gpio.su ./Core/Src/tm_stm32_onewire.d ./Core/Src/tm_stm32_onewire.o ./Core/Src/tm_stm32_onewire.su ./Core/Src/tm_stm32_rcc.d ./Core/Src/tm_stm32_rcc.o ./Core/Src/tm_stm32_rcc.su
+	-$(RM) ./Core/Src/ds18b20.d ./Core/Src/ds18b20.o ./Core/Src/ds18b20.su ./Core/Src/fatfs_sd.d ./Core/Src/fatfs_sd.o ./Core/Src/fatfs_sd.su ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/onewire.d ./Core/Src/onewire.o ./Core/Src/onewire.su ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_hal_timebase_tim.su ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su
 
 .PHONY: clean-Core-2f-Src
 
